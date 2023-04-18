5336705f3fe58b6a0f24260f3137918d79ab1a99
Sebastian Vidrea <vidreasebastian@gmail.com>
2023-04-18 15:50:56 +0300

    Initial commit -- Project setup

---
+++ EmojiApp/EmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAppApp.swift
+//  EmojiApp
+//
+//  Created by Sebastian Vidrea on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
