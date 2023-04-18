4f3ad9c36197bb4cb7686f42c175458be8107e40
Sebastian Vidrea <vidreasebastian@gmail.com>
2023-04-18 16:17:51 +0300

    Test getResponse delivers error on invalid URL

--- EmojiAppTests/APIServiceEndToEndTests.swift
+++ EmojiAppTests/APIServiceEndToEndTests.swift
@@ -7,8 +7,32 @@
 
 import XCTest
 
+final class APIService {
+	private let session: URLSession
+
+	init(session: URLSession = .shared) {
+		self.session = session
+	}
+
+	func getResponse(for url: URL) async throws -> (Data, URLResponse) {
+		try await URLSession(configuration: .default)
+			.data(from: url)
+	}
+}
+
 final class APIServiceEndToEndTests: XCTestCase {
 
+	func test_getResponse_deliversErrorOnInvalidURL() async {
+		let sut = APIService(session: .init(configuration: .ephemeral))
+		let someInvalidURL = URL(string: "some-invalid-url")!
 
+		do {
+			_ = try await sut.getResponse(for: someInvalidURL)
+			XCTFail("Expected th throw an error")
+		} catch {
+			let receivedError = error as NSError
+			XCTAssertEqual(receivedError.code, -1002)
+		}
+	}
 
 }
