3d340bd855647212a655b07821c89d59d02eeee2
Sebastian Vidrea <vidreasebastian@gmail.com>
2023-04-18 15:52:26 +0300

    Add tests class

---
+++ EmojiAppTests/UnicodeScalarTests.swift
@@ -0,0 +1,14 @@
+//
+//  UnicodeScalarTests.swift
+//  EmojiAppTests
+//
+//  Created by Sebastian Vidrea on 16.04.2023.
+//
+
+import XCTest
+
+final class UnicodeScalarTests: XCTestCase {
+
+
+
+}
