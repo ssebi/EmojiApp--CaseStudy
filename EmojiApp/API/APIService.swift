2448401dcaf2992d25c5e530827fb15a4bd2a03e
Sebastian Vidrea <vidreasebastian@gmail.com>
2023-04-18 16:17:26 +0300

    Add APIServiceEndToEndTests

---
+++ EmojiApp/API/APIService.swift
@@ -0,0 +1,8 @@
+//
+//  APIService.swift
+//  EmojiApp
+//
+//  Created by Sebastian Vidrea on 18.04.2023.
+//
+
+import Foundation
---
+++ EmojiAppTests/APIServiceEndToEndTests.swift
@@ -0,0 +1,14 @@
+//
+//  APIServiceEndToEndTests.swift
+//  EmojiAppTests
+//
+//  Created by Sebastian Vidrea on 18.04.2023.
+//
+
+import XCTest
+
+final class APIServiceEndToEndTests: XCTestCase {
+
+
+
+}
