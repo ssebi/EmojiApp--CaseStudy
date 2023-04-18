d65c3131bf4267c0339cbf4ff9b4a344619bffea
Sebastian Vidrea <vidreasebastian@gmail.com>
2023-04-18 16:00:27 +0300

    Add Emoji model

---
+++ EmojiApp/Models/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiApp
+//
+//  Created by Sebastian Vidrea on 18.04.2023.
+//
+
+import Foundation
+
+struct Emoji {
+	let name: String
+	let category: String
+	let group: String
+	let value: String
+}
