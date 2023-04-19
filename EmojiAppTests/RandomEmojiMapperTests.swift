bdf0ac2444d87b3ff9241d94acf1d9b2203a8104
Sebastian Vidrea <vidreasebastian@gmail.com>
2023-04-19 14:56:39 +0300

    Add RandomEmojiMapperTests file

---
+++ EmojiAppTests/RandomEmojiMapperTests.swift
@@ -0,0 +1,12 @@
+//
+//  RandomEmojiMapperTests.swift
+//  EmojiAppTests
+//
+//  Created by Sebastian Vidrea on 19.04.2023.
+//
+
+import XCTest
+
+final class RandomEmojiMapperTests: XCTestCase {
+
+}
