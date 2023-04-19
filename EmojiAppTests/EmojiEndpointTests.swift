9e251c56cb86d0efcd3e96bfe3d2549fb5afcd3a
Sebastian Vidrea <vidreasebastian@gmail.com>
2023-04-19 14:53:13 +0300

    Test random emoji endpoint url

--- EmojiAppTests/EmojiEndpointTests.swift
+++ EmojiAppTests/EmojiEndpointTests.swift
@@ -7,8 +7,27 @@
 
 import XCTest
 
+enum RandomEmojiEndpoint {
+	case get
+
+	func url(baseURL: URL) -> URL {
+		switch self {
+			case .get:
+				return baseURL.appending(path: "api/random")
+		}
+	}
+}
+
 final class EmojiEndpointTests: XCTestCase {
 
+	func test_randomEmoji_endpointURL() {
+		let baseURL = URL(string: "http://base-url.com")!
 
+		let received = RandomEmojiEndpoint.get.url(baseURL: baseURL)
+
+		XCTAssertEqual(received.scheme, "http", "scheme")
+		XCTAssertEqual(received.host, "base-url.com", "host")
+		XCTAssertEqual(received.path, "/api/random", "path")
+	}
 
 }
