ed2c777bab2799a9e06d6322fc46ab43f06dffa6
Sebastian Vidrea <vidreasebastian@gmail.com>
2023-04-19 14:48:05 +0300

    Add EmojiEndpointTests file

---
+++ EmojiAppTests/EmojiEndpointTests.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiEndpointTests.swift
+//  EmojiAppTests
+//
+//  Created by Sebastian Vidrea on 19.04.2023.
+//
+
+import XCTest
+
+final class EmojiEndpointTests: XCTestCase {
+
+
+
+}
