61d539f2f80df4021ade0863c322392206b425c2
Sebastian Vidrea <vidreasebastian@gmail.com>
2023-04-21 12:44:56 +0300

    Test map throws expected error on non 200 http response

--- EmojiAppTests/RandomEmojiMapperTests.swift
+++ EmojiAppTests/RandomEmojiMapperTests.swift
@@ -7,6 +7,44 @@
 
 import XCTest
 
-final class RandomEmojiMapperTests: XCTestCase {
+public enum MappingError: Error {
+	case invalidStatusCode
+}
 
+struct RandomEmojiMapper {
+	static func map(_ data: Data, from response: HTTPURLResponse) throws {
+		guard response.statusCode == 200 else {
+			throw MappingError.invalidStatusCode
+		}
+	}
+}
+
+final class RandomEmojiMapperTests: XCTestCase {
+	
+	func test_map_throwsExpectedErrorOnNon200HTTPResponse() {
+		let data = Data()
+		
+		let samples = [199, 201, 300, 400, 500]
+		
+		samples.forEach { code in
+			do {
+				let _ = try RandomEmojiMapper.map(data, from: HTTPURLResponse(statusCode: code))
+				XCTFail("Expected to throw error")
+			} catch let receivedError as NSError {
+				XCTAssertEqual(receivedError, MappingError.invalidStatusCode as NSError)
+			}
+		}
+	}
+	
+}
+
+// MARK: - Helpers
+func anyURL() -> URL {
+	URL(string: "https://any-url.com")!
+}
+
+extension HTTPURLResponse {
+	convenience init(statusCode: Int) {
+		self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+	}
 }
