93247cd7bd307a81ca9831a6c40eb25dee250c64
Sebastian Vidrea <vidreasebastian@gmail.com>
2023-04-21 13:40:08 +0300

    Add  class

---
+++ EmojiAppTests/Presentation/EmojiViewModelTests.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiViewModelTests.swift
+//  EmojiAppTests
+//
+//  Created by Sebastian Vidrea on 21.04.2023.
+//
+
+import XCTest
+
+final class EmojiViewModel {
+
+}
+
+final class EmojiViewModelTests: XCTestCase {
+
+	func test_init() {
+		let _ = EmojiViewModel()
+	}
+
+}
